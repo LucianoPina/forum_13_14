858429b034d821a066bc92bcfa6dad19c713d68d
leandrix1407 <182757422+leandrix1407@users.noreply.github.com>
2024-11-29 10:30:33 +0000

    Exercicio 1
    No painel de navegação, apenas os administradores podem aceder ao submenu Administração

--- navegacao.php
+++ navegacao.php
@@ -1,5 +1,5 @@
 <?php
-    include_once('verificar_sessao.php');
+include_once('verificar_sessao.php');
 ?>
 <nav class="navbar bg-navegacao rounded">
     <div class="container-fluid">
@@ -7,7 +7,7 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" >
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar">
             <div class="offcanvas-header">
                 <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Opções</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
@@ -18,15 +18,23 @@
                     <button class="btn btn-outline-success" type="submit">Pesquisar</button>
                 </form>
                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
-                            Administração
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="criar_utilizador.php">Criar utilizador</a></li>
-                            <li><a class="dropdown-item" href="utilizadores.php">Ver utilizadores</a></li>
-                        </ul>
-                    </li>
+
+                    <?php
+                    if ($_SESSION['tipo'] == 0) {
+                    ?>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                                Administração
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="criar_utilizador.php">Criar utilizador</a></li>
+                                <li><a class="dropdown-item" href="utilizadores.php">Ver utilizadores</a></li>
+                            </ul>
+                        </li>
+                    <?php
+                    }
+                    ?>
+
                     <li class="nav-item">
                         <a class="nav-link" href="criar_topico.php">Criar tópico</a>
                     </li>
@@ -44,5 +52,3 @@
         </div>
     </div>
 </nav>
-
-
